ad993f94c2810d030fda37f5169c88bbb21a45af
Zeeman <hamza_zz@yahoo.com>
2012-09-30 16:18:23 -0400

    tests

--- src/test/java/com/sporting/misc/BaseIntegrationTest.java
+++ src/test/java/com/sporting/misc/BaseIntegrationTest.java
@@ -4,11 +4,8 @@ import java.io.File;
 
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
-import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
-import org.jboss.shrinkwrap.resolver.api.maven.filter.ScopeFilter;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 
 public class BaseIntegrationTest {
 
@@ -16,23 +13,16 @@ public class BaseIntegrationTest {
 	}
 
 	public static Archive<WebArchive> createTestArchive() {
-
-		final WebArchive war = ShrinkWrap
-				.create(WebArchive.class, "test.war")
-				.setWebXML(new File("src/main/webapp/WEB-INF/web.xml"))
-				.addPackages(true, "com.sporting")
-				.addPackages(true, "org.drools")
-				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+		File[] libs = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeAndTestDependencies().as(File.class);
+		final WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
+				.setWebXML(new File("src/main/webapp/WEB-INF/web.xml")).addPackages(true, "com.sporting")
+				.addPackages(true, "org.drools").addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
 				.addAsResource("test-validation.xml", "META-INF/validation.xml")
 				.addAsResource("import-test.sql", "import.sql")
-				.addAsResource("messages.properties")
 				.addAsWebInfResource(new File("src/main/webapp/WEB-INF/pretty-config.xml"))
 				.addAsWebInfResource(new File("src/main/webapp/WEB-INF/beans.xml"), "beans.xml")
 				.addAsWebInfResource(new File("src/main/webapp/WEB-INF/faces-config.xml"), "faces-config.xml")
-				.addAsLibraries(
-						DependencyResolvers.use(MavenDependencyResolver.class).goOffline().loadEffectivePom("pom.xml")
-								.artifact("org.jboss.shrinkwrap.resolver:shrinkwrap-resolver-impl-maven:2.0.0-alpha-1")
-								.resolveAs(JavaArchive.class, new ScopeFilter("compile", "runtime", "test")));
+				.addAsLibraries(libs);
 
 		// .exclusions("com.google.guava:guava:jar:r07",
 		// "com.google.collections:google-collections:jar:1.0", "dom4j:dom4j:jar:1.6.1",
